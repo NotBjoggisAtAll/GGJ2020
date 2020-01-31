cf27ce822871d63f8f0aa52652da754930cf6aba
Jonas Andersen <20195223+NotBjoggisAtAll@users.noreply.github.com>
2020-01-31 21:20:54 +0100

    Empty Project

---
+++ Source/GGJ2020/GGJ2020GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GGJ2020GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ2020_API AGGJ2020GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
