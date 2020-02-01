1645d90f361f2c30fd672f781cb65816287fb115
Kristoffer Skau <kskau93@gmail.com>
2020-02-01 13:14:55 +0100

    Fix

--- Source/GGJ2020/Interactables/InteractableDiagonalJump.cpp
+++ Source/GGJ2020/Interactables/InteractableDiagonalJump.cpp
@@ -6,6 +6,10 @@
 #include "Components/PrimitiveComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 
+AInteractableDiagonalJump::AInteractableDiagonalJump()
+{
+
+}
 
 void AInteractableDiagonalJump::OnInteract(ABuilderBob* Bob)
 {
@@ -18,7 +22,28 @@ void AInteractableDiagonalJump::OnInteract(ABuilderBob* Bob)
 	comp->SetSimulatePhysics(true);
 	comp->SetConstraintMode(EDOFMode::XZPlane);
 
+
+	auto xVel = (bRotateRight) ? SimulatedMoveSpeed : -SimulatedMoveSpeed;
+	comp->SetPhysicsLinearVelocity({ xVel, 0, 0 });
+
 	comp->AddImpulse(GetActorUpVector() * ImpulseStrength, NAME_None, true);
 
 	Bob->bMoveRight = !Bob->bMoveRight;
 }
+
+#if WITH_EDITOR
+void AInteractableDiagonalJump::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	FName PropertyName = (PropertyChangedEvent.Property != nullptr) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+
+	if (PropertyName == NAME_None)
+		return;
+
+	if ((PropertyName == GET_MEMBER_NAME_CHECKED(AInteractableDiagonalJump, bRotateRight)))
+	{
+		SetActorRotation(FRotator((bRotateRight) ? -45.f : 45.f, 0, 0));
+	}
+}
+#endif
--- Source/GGJ2020/Interactables/InteractableDiagonalJump.h
+++ Source/GGJ2020/Interactables/InteractableDiagonalJump.h
@@ -13,10 +13,23 @@ UCLASS()
 class GGJ2020_API AInteractableDiagonalJump : public ABaseInteractable
 {
 	GENERATED_BODY()
+
+public:
+	AInteractableDiagonalJump();
 	
 protected:
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent);
+#endif
+
 	UPROPERTY(EditAnywhere)
 	float ImpulseStrength = 700.f;
+
+	UPROPERTY(EditAnywhere)
+	float SimulatedMoveSpeed = 300.f;
+
+	UPROPERTY(EditAnywhere)
+	bool bRotateRight = true;
 
 	void OnInteract(ABuilderBob* Bob) override;
 
